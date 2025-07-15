26938b58272dfe2bd9832e210c7a959af1f63c0e
AliReza <alirezatnalireza1@gmail.com>
2025-07-15 18:28:12 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'bg-page': '#171923',
+        'bg-header': '#2d3748',
+      },
+      fontFamily: {
+        vazir: ['Vazirmatn', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
